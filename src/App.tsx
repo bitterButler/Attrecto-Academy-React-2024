02e0dbb5ce94e85f45a5868f54b764ff5eecd959
bitterButler <b.hudson.mark@gmail.com>
2024-09-23 17:22:54 +0200

    read and updated things which were commented. fixed the misunderstanding of the task

--- src/App.tsx
+++ src/App.tsx
@@ -6,16 +6,25 @@ import { Button } from "./components/button/Button";
 function App() {
   const [count, setCount] = useState(0);
   const [error, setError] = useState("");
-
+  const [isDecreaseDisabled, setDecreaseDisabled] = useState(false);
   const updateCounter = (increase: boolean) => {
     setCount((currentValue) => {
-      if (count > 0 && !increase || increase) { //
+      setDecreaseDisabled(false); //toggle for disabling button.
+      if (increase) {
         setError("");
+        return currentValue + 1;
       }
-      if (!increase && count === 1) {
-        setError("cannot go below zero. nice try tho.");
+      else {
+        if (currentValue > 0) {
+          setError("");
+          return currentValue - 1;
+        }
+        else {
+          setError("cannot go below zero. nice try tho.");
+          setDecreaseDisabled(true); // set toogle to be disabled.
+          return 0;
+        }
       }
-      return increase ? currentValue + 1 : currentValue - 1; //other easier/faster solution: Math.max(0, currentValue-1)
     });
   };
 
@@ -27,13 +36,12 @@ function App() {
       <div className="container d-flex justify-content-center">
         <div className="card bg-white shadow text-center p-4 m-4">
           <h1>Counter: {count}</h1>
-          <div><p style={{ color: 'red' }}>{error}</p></div>
+          <div><p className="text-danger">{error}</p></div>
           <div className="d-flex justify-content-center flex-wrap gap-2">
             <Button color="primary" onClick={() => updateCounter(true)}>Increase +</Button>
-            <Button color="secondary" onClick={() => updateCounter(false)} disabled={count === 0}>Decrease -</Button>
-            <Button color="danger" onClick={() => { setCount(0); setError(""); }}>Reset</Button>
+            <Button color="secondary" onClick={() => updateCounter(false)} disabled={isDecreaseDisabled}>Decrease -</Button>
+            <Button color="danger" onClick={() => { setCount(0); setError(""); setDecreaseDisabled(false) }}>Reset</Button>
           </div>
-
         </div>
       </div>
     </div>
