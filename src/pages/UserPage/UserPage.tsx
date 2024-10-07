ef80e0ccc8e571b000b1143e3f77622da7e2a5a5
bitterButler <b.hudson.mark@gmail.com>
2024-10-07 21:26:28 +0200

    session-3-extra-task

---
+++ src/pages/UserPage/UserPage.tsx
@@ -0,0 +1,63 @@
+import React, { useState } from "react";
+import { useParams } from "react-router-dom";
+import * as Yup from "yup";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+
+import { Page } from "../../components/page/Page";
+import { UserFormValues, UserModel } from "../../models/user.model";
+import { BadgeModel } from "../../models/badges.model";
+import TextField from "../../components/text-field/TextField";
+import { ProfileImagePreview } from "../../components/page/ProfileImagePreview";
+
+const schema = Yup.object({
+  name: Yup.string().required(),
+  image: Yup.string().required(),
+});
+
+export const UserPage = () => {
+  const { id } = useParams<{ id: string }>();
+  const [user, setUser] = useState<UserModel>();
+  const [badges, setBadges] = useState<BadgeModel[]>([]);
+
+  const {
+    register,
+    control,
+    handleSubmit,
+    reset,
+    setValue,
+    getValues,
+    watch,
+    formState: { errors },
+  } = useForm<UserFormValues>({
+    defaultValues: {
+      name: "",
+      image: "",
+    },
+    resolver: yupResolver(schema),
+  });
+
+  const ImageURL = watch("image");
+
+  return (
+    <Page title={user ? user.name : "User"}>
+      <form>
+        <TextField
+          name="name"
+          label="Name"
+          register={register}
+          error={errors.name?.message}
+        />
+        <TextField
+          name="image"
+          label="Avatar url"
+          register={register}
+          error={errors.image?.message}
+        />
+        <ProfileImagePreview
+          imageURL={ImageURL}
+        />  
+      </form>
+    </Page>
+  );
+};
